8355e709678d942f250b17ae7fef4310a52c4683
Godwin Okpe <okpegodwin18@yahoo.com>
2023-02-21 12:16:06 -0800

    Nexting functions is tough but interesting

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Shows 1 if the input is a
+ * letter Another cases, shows 0
+ *
+ * n: The character in ASCII code
+ *
+ * Return: 1 for letters. 0 for the rest.
+ */
+int _isalpha(int n)
+{
+	if ((n >= 97 && n <= 122) || (n >= 65 && n <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
