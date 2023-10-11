967574d1001028fd0f8cb1d6f8bc22e5ea631ec1
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-10-11 13:24:01 -0700

    Adding the logic for binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
  *binary_search - binary function to find the index of a value
@@ -10,4 +11,39 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	int low, mid, high, i;
+
+	if (array == NULL)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		} else if (array[mid] < value)
+		{
+			low = mid + 1;
+		} else
+		{
+			high = mid - 1;
+		}
+	}
+	return (-1);
+}
+
 
